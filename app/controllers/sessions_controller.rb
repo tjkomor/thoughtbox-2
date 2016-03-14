68ba9fe00470091ac46d06f7a25a093c10639675
Tyler Komoroske <tjkomor@clemson.edu>
2016-03-14 13:34:07 -0600

    add logout path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,11 @@ class SessionsController < ApplicationController
     end
   end
 
+  def delete
+    session.delete(:user_id)
+    redirect_to root_path
+  end
+
   private
 
   def session_params
